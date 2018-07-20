e1e42a705689704bcc749f10e7346dcee09a3281
cscov <carolyn.l.scoville@gmail.com>
2018-07-20 12:12:22 -0700

    Blocking and non-blocking code examples

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// console.log("Hello, World!");
 var http = require("http");
 
 http.createServer(function (request, response) {
@@ -11,3 +10,20 @@ http.createServer(function (request, response) {
 }).listen(8081);
 
 console.log("Server is running at http://127.0.0.1:8081/");
+
+// Blocking Code Example
+// var fs = require("fs");
+// var data = fs.readFileSync('input.txt');
+//
+// console.log(data.toString());
+// console.log("Program Ended");
+
+// Non-Blocking Code Example
+var fs = require("fs");
+
+fs.readFile('input.txt', function (err, data) {
+  if (err) return console.error(err);
+  console.log(data.toString());
+});
+
+console.log("Program Ended");
