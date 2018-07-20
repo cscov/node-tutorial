2cb19c79538e1f608622540862a6dead58b14f50
cscov <carolyn.l.scoville@gmail.com>
2018-07-20 11:49:04 -0700

    First successful server request

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("Hello, World!");
+// console.log("Hello, World!");
+var http = require("http");
+
+http.createServer(function (request, response) {
+  // Send the HTTP header
+  // HTTP Status: 200 : OK
+  // Content Type: text/plain
+  response.writeHead(200, {'Content-type': 'text/plain'});
+  // Send the response body as 'Hello World'
+  response.end('Hello World\n');
+}).listen(8081);
+
+console.log("Server is running at http://127.0.0.1:8081/");
